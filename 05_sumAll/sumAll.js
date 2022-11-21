d435a3e06f1a35f390490deb72ad0db0275d464e
Thomas Dinh <thomas98dinh@gmail.com>
2022-11-20 23:48:39 -0800

    Add and test solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let finalSum = 0;
+    if(num1 < 0 || num2 < 0 || Number(num1) !== num1 || Number(num2) !== num2) {
+        return "ERROR";
+    }
+
+    if(num1 < num2) {
+    for(let i = num1; i <= num2; i++) {
+        finalSum += i;
+        }
+    } else if (num1 > num2) {
+    for(let i = num2; i <= num1; i++) {
+        finalSum += i;
+        }
+    }
+    return finalSum;
+    
 };
 
 // Do not edit below this line
