f114c5f8d3698e5b4ad68c3c68d2bbf2f898d817
Thomas Dinh <thomas98dinh@gmail.com>
2022-11-20 23:30:37 -0800

    Add and test solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
 
+    for(const arg of theArgs) {
+        if(!array.includes(arg)) {
+            continue;
+        }
+        const index = array.indexOf(arg);
+        const removed = array.splice(index, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
