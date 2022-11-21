0d9401f31d9389fa80e315ecf710ccf263dcb489
Thomas Dinh <thomas98dinh@gmail.com>
2022-11-20 22:34:25 -0800

    Complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    let repeated = "";
+    if(num < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+        repeated += string;
+
+    }
+    return repeated;
 
 };
 
