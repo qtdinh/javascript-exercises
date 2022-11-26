ebd8dccdd1d67716d05cdd2995140c81af6a9b35
Thomas Dinh <thomas98dinh@gmail.com>
2022-11-26 00:57:23 -0800

    Complete and test exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+     return array.reduce(function(a, b) {
+        // return lastGuy > nextGuy ? -1 : 1;
+        const oldestAge = getAge(a.yearOfBirth, a.yearOfDeath);
+        const currentAge = getAge(b.yearOfBirth, b.yearOfDeath);
+        return oldestAge < currentAge ? b : a;
+})
 };
+
+function getAge (birth, death) {
+
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
