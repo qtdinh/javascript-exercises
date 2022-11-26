c1b6a998a14404c6088460aa022fbcebb80b4e83
Thomas Dinh <thomas98dinh@gmail.com>
2022-11-25 22:30:02 -0800

    Complete and text exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    //remove all non-alphanumeric characters
+    //turn string into lowercase
+    //make another string that is reversed
+    //compare both strings
+    //return true if palindrome, if not false
+    let re = /\W+/g;
 
+    let regularStr = str.toLowerCase().replace(re, '');
+
+    let reverseStr = regularStr.split('').reverse().join('');
+
+    if(regularStr === reverseStr) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
