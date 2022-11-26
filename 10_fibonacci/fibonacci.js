b8a0d9f2981010fe7adefab03760d9aa5646dddd
Thomas Dinh <thomas98dinh@gmail.com>
2022-11-25 22:52:57 -0800

    Complete and test exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let currentResult = 1;
+    let oldNumber = 0;
+    let nextTerm = currentResult + oldNumber;
+
+    if(num < 0) {
+        return 'OOPS';
+    }
+
+    for(let i = 2; i <= num; ++i) {
+        oldNumber = currentResult;
+        currentResult = nextTerm;
+        nextTerm = currentResult + oldNumber;
+    }
+
+    return currentResult;
 
 };
 
