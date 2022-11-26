3c8a5f65d08bbb5fe081cf28b269c7adecf538ef
Thomas Dinh <thomas98dinh@gmail.com>
2022-11-25 22:14:54 -0800

    Complete and test exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0;
+	for(let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let multiplied = 1;
+	for(let i = 0; i < numbers.length; i++) {
+    multiplied *= numbers[i];
+  }
+  return multiplied;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0) {
+    return 1;
+  } else {
+
+	for(let i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+ }
+  return num;
 };
 
 // Do not edit below this line
